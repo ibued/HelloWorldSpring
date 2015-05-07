7520c19e15c791c84817c74eb91a7ec026c534d2
ibued <javaphp65@gmail.com>
2015-05-07 10:15:15 +0700

    Create UserRepository.java

---
+++ UserRepository.java
@@ -0,0 +1,22 @@
+# HelloWorldSpring
+package org.com.databarang.repository;
+import java.util.Collection;
+
+import org.springframework.dao.DataAccessException;
+import org.com.databarang.model.dataUser;
+
+public interface UserRepository {
+
+  Collection<dataUser> getAll() throws DataAccessException;
+    
+  dataUser editUser(String id) throws DataAccessException;
+
+	void simpan(dataUser dataUser) throws DataAccessException;
+	
+	public void simpanUser(dataUser user) throws DataAccessException;
+	
+	public dataUser hapus(String id) throws DataAccessException;
+	
+	public void hapusUser(String id) throws DataAccessException;
+  
+}
