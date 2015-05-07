353d8fd44ec216cc06d62d30f6227c0f2d46590f
ibued <javaphp65@gmail.com>
2015-05-07 10:12:48 +0700

    Create UserRowMapper.java

---
+++ UserRowMapper.java
@@ -0,0 +1,22 @@
+# HelloWorldSpring
+
+package org.unirow.databarang.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class DatauserRowMapper implements RowMapper<DataUser> {
+
+	@Override
+	public DataUser mapRow(ResultSet rs, int rownumber) throws SQLException {
+		DataUser obj = new DataUser();
+		obj.setIduser(rs.getString("iduser"));
+		obj.setNamauser(rs.getString("namauser"));
+		obj.setAlamat(rs.getString("alamat"));
+		obj.setTelp(rs.getString("telp"));
+		return obj;
+	}
+
+}
